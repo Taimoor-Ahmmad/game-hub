5296ddaf33c2f59c4dd3c71573d1698c37e212dc
Timmy530 <taimoorahmadofficial530@gmail.com>
2023-09-21 19:52:57 +0500

    Installed Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+
+import {Button} from '@chakra-ui/react'
+
+
+
+
 function App() {
-  return <div>Hello world</div>;
+  return <Button colorScheme='blue'>Button</Button>;
 }
 
 export default App;
